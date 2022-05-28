4346785b51f89b0ad7426b31d85dbf5a9f4062ae
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-05-28 22:42:46 +0300

    new file

---
+++ anagram.py
@@ -0,0 +1,20 @@
+# Check if a word is an anagrams
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+    if len(word) != len(anagram):
+        return False
+
+    word = sorted(word)
+    anagram = sorted(anagram)
+
+    return word == anagram
+
+
+word = "hello"
+anagram = "racecar"
+print(find_anagrams(word, anagram))
