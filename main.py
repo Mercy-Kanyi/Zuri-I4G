6ae1b3a85a4ed99f5fb5538ba0a631babf59486c
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-06-02 23:50:05 +0300

    updated objects and methods

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
-#from msilib.schema import SelfReg
-#from typing_extensions import Self
-
-
 class Student:
-
     # [assignment] Skeleton class. Add your code here
-
-
-    def _init_(self, Name, age, tracks, score):
-        self.Name = Name
+    def __init__(self, name, age, tracks, score):
+        self.name = name
         self.age = age
         self.tracks = tracks
         self.score = score
-        #create the first object
-        #object of student
+
+    def change_name(self, name):
+        print("My new name is", name)
+
+    def change_age(self, age):
+        print("My age has changed to", age)
+
+    def add_track(self, track):
+        print("I have changed my track to", track)
+
+    def get_score(self):
+        print("I scored", self.score)
 
 
-class Student:
-    Name = "Bob"
-    age = 34
-    tracks = "UI/UX"
-    score = 10.11
-    print('class student: ')
-    print('Name is a', Name)
-    print('age is a', age)
-    print('track is a', tracks)
-    print('score is a', score)
+Bob = Student("Bob", 26, ["FE", "BE"], 20.90)
 
-
-print("\nAccessing class variable using class name")
-#print(Self.name)
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
